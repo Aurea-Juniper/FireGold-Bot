4983bc7ecdb97bb578e70abd6db5144c74de879c
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-12 19:41:41 +0530

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,34 @@
+module.exports ={ 
+  name:"help",
+  aliases:["h", "help", "info"],
+  code:`
+$title[Commands list]
+$description[
+• \`!tm <01-50>\`
+**Location of a TM**
+
+• \`!hm <01-08>\`
+**Location of an HM.**
+
+• \`!loc <pokemon>\`
+ **Locations of a Pokémon.**
+
+• \`!learnset <pokemon>\`
+**Level up moves of a Pokémon.**
+
+• \`!eggmoves <pokemon>\`
+**Egg moves of a Pokémon.**
+
+• \`!helditem <item_name>\`
+**Pokémon that may carry said item.** 
+
+• \`!bpitems\`
+**Cost of all items in the Battle Tower.**
+
+• \`!download\`
+**Gives link to the latest patch.**
+
+• \`!docs\`
+**Gives link to the game's documentations.**]
+$suppressErrors`
+}
