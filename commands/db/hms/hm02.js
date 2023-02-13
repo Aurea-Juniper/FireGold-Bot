07617a38df0cfc116b414585a6b5e8cc1e27ab7d
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-13 17:43:06 +0530

    Create hm02.js

---
+++ commands/db/hms/hm02.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm02",
+  aliases: ["HM02", "HM 02", "hm02", "hm 02"],
+  code: `
+  $author[HM #02]
+  $title[Fly]
+  $description[			Given by Chuck's Wife outside the Cianwood City Gym after beating Chuck.]
+  `
+}
