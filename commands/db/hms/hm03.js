e17f26b1b3f94b0b8b83065410a62563af9fd65c
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-13 17:44:57 +0530

    Create hm03.js

---
+++ commands/db/hms/hm03.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm03",
+  aliases: ["HM03", "HM 03", "hm03", "hm 03"],
+  code: `
+$author[HM #03]
+$title[Surf]
+$description[			Given by the Gentleman in the Ecruteak Dance Theater after beating the Kimono Girls.]
+  `
+}
