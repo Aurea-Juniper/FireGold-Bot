3d32ef6e50bb555f487dd3df3e15153a6872f3bb
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-14 16:37:44 +0530

    Create hm08.js

---
+++ commands/db/hms/hm08.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm08",
+  aliases: ["hm08", "HM08", "hm 08", "HM 08"],
+  code: `
+$author[HM #08]
+$title[Whirlpool]
+$description[			Given by Lance in Team Rocket's Hideout in Mahogany Town.]
+`
+} 
