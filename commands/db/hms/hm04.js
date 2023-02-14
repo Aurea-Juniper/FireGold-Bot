1a271762230fb5477b41e4c0d9f240bbe790302f
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-14 16:33:07 +0530

    Create hm04.js

---
+++ commands/db/hms/hm04.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm04",
+  aliases: ["hm04", "HM04", "hm 04", "HM 04"],
+  code: `
+$author[HM #04]
+$title[Strength]
+$description[			Given by the Sailor in Olivine City left of the Pokemon Center.]
+`
+} 
