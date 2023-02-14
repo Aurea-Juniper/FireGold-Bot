89e358baf3df789c67be503d02c5833bbcc6228a
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-14 16:35:02 +0530

    Create hm06.js

---
+++ commands/db/hms/hm06.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm06",
+  aliases: ["hm06", "HM06", "hm 06", "HM 06"],
+  code: `
+$author[HM #06]
+$title[Rock Smash]
+$description[			Given by the Chubby Guy on Route 36 after using the SquirtBottle on Sudowoodo.]
+`
+} 
