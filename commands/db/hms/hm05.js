7135130bbe4bf74742943844bf05e33172f7e303
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-14 16:33:37 +0530

    Create hm05.js

---
+++ commands/db/hms/hm05.js
@@ -0,0 +1,9 @@
+module.exports ={
+  name: "hm05",
+  aliases: ["hm05", "HM05", "hm 05", "HM 05"],
+  code: `
+$author[HM #05]
+$title[Flash]
+$description[			Reward for beating Elder in Sprout Tower.]
+`
+}
