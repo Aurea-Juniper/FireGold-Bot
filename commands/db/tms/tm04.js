1576ce4da166d5389352bf90c2c0363333b0d2bb
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-24 16:38:09 +0530

    Create tm04.js

---
+++ commands/db/tms/tm04.js
@@ -0,0 +1,10 @@
+module.exports ={
+  name: "tm04",
+  aliases: ["tm 04"],
+  code:`
+$deletecommand
+$author[TM #04]
+$title[Calm Mind]
+$description[        Reward for beating Gym Leader Sabrina.] 
+`
+} 
