3eb477942041899f9a358665cb2feb9e83b040cd
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-02-26 15:16:05 +0530

    MEGA UPDATE!
    - Upgrading to aoi.js v6.0.3

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-// Loading express server
-const express = require('express')
-const app = express()
-const port = 3000
+const aoijs = require("aoi.js") // installing aoi.js
+console.log('SYSTEM :: Installing aoi.js')
+const bot = new aoijs.AoiClient({ // creating new client
+token: process.env.TOKEN, // bot token
+prefix: "!", // bot prefix
+intents: ["MessageContent", "Guilds", "GuildMessages"]
+}) // client intents
+bot.onMessage() // client events
+const loader = new aoijs.LoadCommands(bot) // load command
+loader.load(bot.cmd,"./src/commands/") // loading commands
+//require('.src/handler/status')(bot) // loading handlers
+const express = require('express'); // loading express
+const app = express(); // defining app = express
 app.get('/', (req, res) => {
-  res.send('Express web server is running!')
-})
-app.listen(port, () => {
-console.log(`Currently listening to localhost:${port}`)
-}) 
-// installing aoi.js
-const aoijs = require("aoi.js")
-const bot = new aoijs.AoiClient({
-  token: process.env.TOKEN, // bot token
-  prefix: "!", //bot prefix
-  intents: ["GUILDS", "GUILD_MESSAGES"]
-}) // gateway intents
-bot.onMessage()
-const loader = new aoijs.LoadCommands(bot)
-loader.load(bot.cmd,"./commands/") // loading commands
-require('./handler/status')(bot) // loading handlers
+  res.send('Hello Express app!') // send this
+});
+app.listen(3000, () => { // listen to port
+  console.log('SYSTEM :: Express web server is now working!');
+});
