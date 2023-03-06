a929d74ce80cc43f4d83c7f9fb6b020d9d911017
Aurea Juniper <94511889+Aurea-Juniper@users.noreply.github.com>
2023-03-06 21:02:04 +0530

    Update index.js
    [+] updated to latest version of aoi.js
    [+] easier to understand

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const aoijs = require("aoi.js") // installing aoi.js
-console.log('SYSTEM :: Installing aoi.js')
-const bot = new aoijs.AoiClient({ // creating new client
-token: process.env.TOKEN, // bot token
-prefix: "!", // bot prefix
-intents: ["MessageContent", "Guilds", "GuildMessages"]
-}) // client intents
-bot.onMessage() // client events
-const loader = new aoijs.LoadCommands(bot) // load command
-loader.load(bot.cmd,"./src/commands/") // loading commands
-//require('.src/handler/status')(bot) // loading handlers
-const express = require('express'); // loading express
-const app = express(); // defining app = express
+/* ==== Installing required packages ==== */
+const { AoiClient, LoadCommands } = require("aoi.js");
+const express = require("express")
+/* ==== Defining configuration folder ==== */
+const config = ("./config.json");
+/* ==== Creating new aoi.js client ==== */
+const bot = new AoiClient({
+  /* === Bot token === */
+  token: config.client.TOKEN,
+  /* === Bot Prefix === */
+  prefix: config.client.PREFIX,
+  /* === Client intents === */
+  intents: ["GuildMessages", "MessageContent", "Guilds"],
+  /* === Client events === */
+  events: ["onMessage"]
+})
+/* === Bot commands === */
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./commands");
+/* === Bot status === */
+bot.status({
+  text: config.status.text,
+  /* === The text for status === */
+  type: config.status.type, 
+  /* === The type of status ===
+    Valid stuff which can be used:
+    PLAYING, LISTENING, WATCHING or COMPETING */
+  status: config.status.status, 
+  /* === Bot status ===
+  Valid stuff which can be used:
+  dnd, online, idle or offline */
+  // url: 
+});
+/* Creating web server using express */
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Express app!') // send this
+  res.send('Express web server is working!')
 });
-app.listen(3000, () => { // listen to port
-  console.log('SYSTEM :: Express web server is now working!');
-});
+app.listen(config.express.port, () => {
+  console.log('EXPRESS: Express web server is working!');
+})
